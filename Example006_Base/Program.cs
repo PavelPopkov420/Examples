f4dea82f2c9c882cb914e592e49543e4d59ff066
Pavel Popkov <goil2011@yandex.ru>
2022-07-11 22:09:20 +0300

    BASE

---
+++ Example006_Base/Program.cs
@@ -0,0 +1,16 @@
+﻿int a = 3;
+int b = 6;
+int c = 8;
+int d = 2;
+int e = 4;
+
+int max = a;
+
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+
+Console.Write("max = ");
+Console.WriteLine(max);
