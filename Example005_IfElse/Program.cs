8689786239155d66c93fd23edc6014f4f6e10a45
Pavel Popkov <goil2011@yandex.ru>
2022-07-11 21:59:07 +0300

    ifelse

---
+++ Example005_IfElse/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "маша")
+{
+   Console.WriteLine("Ура, Маша привет!");
+}
+
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
